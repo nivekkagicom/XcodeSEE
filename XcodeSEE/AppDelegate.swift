8fea76b35279a43f2e40c4985815db9ae8967c19
B. Kevin Hardman <nivek.research@gmail.com>
2017-09-20 07:26:26 -0400

    Initial Commit

---
+++ XcodeSEE/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XcodeSEE
+//
+//  Created by B. Kevin Hardman on 9/20/17.
+//  Copyright © 2017 B. Kevin Hardman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ XcodeSEE/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XcodeSEE
+//
+//  Created by B. Kevin Hardman on 9/20/17.
+//  Copyright © 2017 B. Kevin Hardman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
